3462921b0f47d4a57d58a9df3ca99038536ee940
hojunJeong <hojun1105@gmail.com>
2025-09-21 17:03:10 +0900

    update

--- src/main/java/org/example/controller/EmailVerificationController.java
+++ src/main/java/org/example/controller/EmailVerificationController.java
@@ -1,15 +1,22 @@
 package org.example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.example.service.EmailService;
 import org.example.service.VerificationService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class EmailVerificationController {
 
     private final EmailService emailService;
     private final VerificationService verificationService;
+    private final Map<String, String> codeStore = new HashMap<>();
+
 
     public EmailVerificationController(EmailService emailService,
                                        VerificationService verificationService) {
@@ -21,14 +28,19 @@ public class EmailVerificationController {
     @ResponseBody
     public String sendCode(@RequestParam("email") String email) {
         String code = verificationService.generateCode(email);
+        codeStore.put(email, code);
         emailService.sendVerificationEmail(email, code);
         return "인증 코드 전송 완료";
     }
 
     @PostMapping(value = "/verifyCode", produces = "text/plain; charset=UTF-8")
     @ResponseBody
-    public String verifyCode(@RequestParam String email, @RequestParam String code) {
-        boolean success = verificationService.verifyCode(email, code);
-        return success ? "인증 성공!" : "인증 실패!";
-    }
+    public ResponseEntity<String> verifyCode(@RequestParam("email") String email, @RequestParam("code") String code) {
+        String storedCode = codeStore.get(email);
+        if(storedCode !=null && storedCode.equals(code)) {
+            return ResponseEntity.ok("인증성공!");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증 실패 ❌");
+        }}
 }
